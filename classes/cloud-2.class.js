40aae0205439f4a5e87ea943793a35cf54bdc5e4
sven-olebittner <sven-ole.bittner@gmx.de>
2026-04-18 14:17:00 +0200

    feat: update camera handling in Character and World classes; add multiple clouds to World

--- classes/cloud-2.class.js
+++ classes/cloud-2.class.js
@@ -3,7 +3,7 @@ class Cloud2 extends Cloud1 {
         super().loadImage(`./img/5_background/layers/4_clouds/2.png`);
         this.height = 480;
         this.width = 720;
-        this.x = -100 + Math.random() * 800;
+        this.x = 800 + Math.random() * 800;
         this.y = -20;
     }
 }
--- classes/world.class.js
+++ classes/world.class.js
@@ -1,17 +1,37 @@
 class World {
   character = new Character();
   bottle = new Bottle();
-  clouds = [new Cloud1()];
+  clouds = [new Cloud1(), new Cloud2(720)];
   coin = new Coin();
   ctx;
   canvas;
   keyboard;
+  camera_x = 0;
 
   backgroundObjects = [
+    new BackgroundObject("img/5_background/layers/air.png", -719),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -719),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", -719),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -719),
     new BackgroundObject("img/5_background/layers/air.png", 0),
     new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
     new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
     new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
+    new BackgroundObject("img/5_background/layers/air.png", 719),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719),
+    new BackgroundObject("img/5_background/layers/air.png", 719 * 2),
+    new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 719 * 2),
+    new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 719 * 2),
+    new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 719 * 2),
+    new BackgroundObject("img/5_background/layers/air.png", 719 * 3),
+    new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719 * 3),
+    new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719 * 3),
+    new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719 * 3),
+
+
+
   ];
   enemys = [
     new LittleChicken(),
@@ -32,12 +52,13 @@ class World {
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    // this.ctx.globalCompositeOperation = "destination-over";
+    this.ctx.translate(this.camera_x, 0);
     this.addObjectsToMap(this.backgroundObjects);
     this.addObjectsToMap(this.clouds);
     this.addObjectsToMap(this.enemys);
 
     this.addToMap(this.character);
+    this.ctx.translate(- this.camera_x, 0);
 
     let self = this;
     // Draw() wird immer wieder aufgerufen, damit die Bewegungen der Enemys sichtbar werden
